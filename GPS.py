c6a6dd92420b2e7e7feeac9bd43981c5c356a139
fojek <marc.fortier.1@gmail.com>
2017-07-17 21:52:12 +0000

    Nouveau script

--- GPS.py
+++ GPS.py
@@ -1,50 +1,57 @@
-# GPS.py
- asdf ads# ------------
-# Acquiert, decortique et publie les données GPS
-# ------------
-# 
-# A faire :
-#  - Connection a la base de donnees mySQL
-#  - Publication des resultats
-#
-# ------------
-
 import serial
 
-# Connection au GPS
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
+def getInfo( str ):
 
-# Tableaux pour decortiquer la chaine GPGGA
-virgule = [0,0,0,0,0,0,0,0,0,0,0,0]
-GPSdata = ['','','','','','','','','','']
+        # Separe aux ','
+        infos = str.split(',')
 
-# Boucle principale
+        # Verifie la validite des coordonnees
+        if len(infos[2]) == 0:
+                return 0
+        
+        # Heure
+        infoGPS['heure'] = int(infos[1][0:2])
+        infoGPS['minute'] = int(infos[1][2:4])
+        infoGPS['seconde'] = int(infos[1][4:6])
 
-while True:
-	
-    # On cherche le caractere de debut de chaine
-    rcv = port.read(1)
-    if rcv is '$':
-		
-		# On cherche la chaine de type GPGGA
-		rcv = port.read(5)
-		if rcv == 'GPGGA':
-			
-			# On ramasse la chaine au complet, et on trouve la position des ','
-			rcv = port.read(70)
-			virgule[0] = rcv.find(',')
-			for i in range(1,11):
-				virgule[i] = rcv.find(',',virgule[i-1]+1)
-			
-			# On extrait les donnees entre les virgules, selon
-			# $GPGGA,212113.00,4524.17242,N,07150.81415,W,1,07,1.48,210.5,M,-31.4,M,,*64
-			# $GPGGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx			
-			for i in range(10):
-				GPSdata[i] = rcv[virgule[i]+1:virgule[i+1]]
-			
-			# Les donnees : Latitude, direction, Longitude, direction, Altitude, unite.
-			# @ publier dans BDD
-			print 'Latitude  : ', float(GPSdata[1])/100, ' ', GPSdata[2]
-			print 'Longitude : ', float(GPSdata[3])/100, ' ', GPSdata[4]
-			print 'Altitude  : ', float(GPSdata[8]), ' ', GPSdata[9]
-			print '----------------'
+        # Latitude, longitude, altitude
+        infoGPS['lat_angle'] = infos[2]
+        infoGPS['lat_coord'] = infos[3]
+        infoGPS['lon_angle'] = infos[4]
+        infoGPS['lon_coord'] = infos[5]
+        infoGPS['altitude'] = infos[11]
+
+        return 1
+
+gps = serial.Serial('/dev/ttyAMA0',9600)
+
+infoGPS = { 'heure' : 0,
+                'minute' : 0,
+                'seconde' : 0,
+                'lat_angle' : 0,
+                'lat_coord' : '',
+                'lon_angle' : 0,
+                'lon_coord' : '',
+                'altitude' : 0}
+
+if not gps.isOpen():
+	print 'Erreur : impossible d ouvrir le port serie.'
+	exit()
+
+print 'Connection etablie.'
+
+while 1:
+        data = gps.readline()
+        
+        if len(data) > 6:
+
+                type = data[0:6]
+                
+                if type == "$GPGGA":
+                        if(getInfo(data[6:len(data)])):
+                                print 'Nouvelles coordonnees : '
+                                print infoGPS
+                        else:
+                                print 'Pas de connection satellite.'
+
+
