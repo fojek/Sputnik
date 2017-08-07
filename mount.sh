9438b0f5cc0346b689ae47590f511579cde926d1
fojek <marc.fortier.1@gmail.com>
2017-08-07 09:32:28 -0500

    maj

---
+++ mount.sh
@@ -0,0 +1,4 @@
+sudo qtcreator -noload Welcome -noload QmlProfiler -noload QmlDesigner
+
+sudo mount -t cifs -o noperm //192.168.0.100/volume_1 /mnt/Nuage/volume_1
+sudo mount -t cifs -o noperm //192.168.0.100/volume_2 /mnt/Nuage/volume_2
